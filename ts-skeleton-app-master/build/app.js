61749bd5217327512c88693ef51559746ca71e5f
CassyBoy2000 <broe0108@hz.nl>
2018-12-12 14:02:23 +0100

    0.0.3

--- ts-skeleton-app-master/build/app.js
+++ ts-skeleton-app-master/build/app.js
@@ -45,38 +45,12 @@ class Block extends Entity {
         super(canvas, imgSource, xPos, yPos, width, height);
     }
 }
-class ButtonAction {
-    constructor(x, y, h, w, fn) {
-        this.x = x;
-        this.y = y;
-        this.h = h;
-        this.w = w;
-        this.fn = fn;
-    }
-    ExecuteIfInArea(x, y) {
-        if (x > this.x && x < this.x + this.w &&
-            y > this.y && y < this.y + this.h) {
-            this.fn();
-        }
-    }
-}
 class Canvas {
     constructor(canvas) {
-        this.clickCommands = new Map();
         this.canvas = canvas;
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext('2d');
-        document.addEventListener('click', (event) => {
-            this.OnClick(event);
-        });
-    }
-    OnClick(Event) {
-        let X = Event.x;
-        let Y = Event.y;
-        this.clickCommands.forEach((value, key) => {
-            value.ExecuteIfInArea(X, Y);
-        });
     }
     clearCanvas() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -113,7 +87,6 @@ class Game {
         this.player = new Player(canvasElement, './assets/images/bluePlayer.png', 200, 200, 100, 10);
         this.block = new Block(canvasElement, './assets/images/blueBlock.png', 50, 50, 40, 40);
         this.startView = new StartView();
-        this.continentView = new ContinentView();
     }
 }
 window.addEventListener('load', init);
@@ -201,15 +174,13 @@ class StartView extends ViewBase {
         super();
         const canvasElement = document.getElementById('canvas');
         this.canvas = new Canvas(canvasElement);
-        this.buttonAction = new ButtonAction(160, 100, 60, 150, this.canvas.clearCanvas);
     }
     createScreen() {
         document.body.style.background = "url('./assets/images/backgrounds/startBackground.png') no-repeat ";
         document.body.style.backgroundSize = "cover";
         document.body.style.zIndex = "-1";
         this.canvas.writeTextToCanvas('World Explorer', 100, this.canvas.getCenter().X, 100, "white", "center");
-        this.canvas.writeImageToCanvas('./assets/images/startscreenButton.png', 160, 100);
-        this.buttonAction.ExecuteIfInArea(160, 100);
+        this.canvas.writeImageToCanvas('./assets/images/startscreenButton.png', this.canvas.getCenter().X - 267, this.canvas.getCenter().Y / 2.2);
     }
 }
 //# sourceMappingURL=app.js.map
--- ts-skeleton-app-master/src/Game.ts
+++ ts-skeleton-app-master/src/Game.ts
@@ -15,20 +15,11 @@ class Game{
         this.ball = new Ball(canvasElement, './assets/images/balls/football.png', 100, 100, 10, 10, 0);
         this.player = new Player(canvasElement, './assets/images/bluePlayer.png', 200, 200, 100, 10);
         this.block = new Block(canvasElement,'./assets/images/blueBlock.png',50,50,40,40);
-<<<<<<< HEAD
         this.startView = new StartView()
     }
 
     draw = () => {
-        this.startView.createScreen();
-=======
-        this.startView = new StartView();
-        this.continentView = new ContinentView();
-    }
-
-    draw = () => {
-        this.continentView.createScreen();  
->>>>>>> 06a65dd558ffc8975452e984636a0289f8eaefd4
+        this.continentView.createScreen();
     }
 }
 
